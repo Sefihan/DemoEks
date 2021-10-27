8eb90b50306b2200578ed3e46f0878e136dbc213
Sefihan <sefihan5800@gmail.com>
2021-10-27 03:45:45 +0300

    Succesful trying

---
+++ DemoEkz/Pages/ProductAdd.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoEkz.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для ProductAdd.xaml
+	/// </summary>
+	public partial class ProductAdd : Page
+	{
+		public ProductAdd()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnSave_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
